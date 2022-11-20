4be621a1120c8627d02346cde73d364dfe515b13
admin <admin@mail.com>
2022-11-20 15:14:14 +0400

    migrations

---
+++ database/migrations/2022_11_19_180427_create_companies_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 125);
+            $table->string('phone', 25);
+            $table->string('description');
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table){
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('companies');
+    }
+};
